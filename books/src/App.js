1bb00e54d989e9be01953fdc693d823893a76213
sarikayacem <79401289+sarikayacem@users.noreply.github.com>
2024-03-13 17:51:05 +0300

    books finished

---
+++ books/src/App.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
+
+function App() {
+  const [books, setBooks] = useState([]);
+
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+    setBooks(updatedBooks);
+  };
+
+  const editBookById = (id, newTitle) => {
+    const editedBooks = books.map((book) => {
+      if (book.id === id) {
+        return { ...books, title: newTitle };
+      }
+      return book;
+    });
+    setBooks(editedBooks);
+  };
+
+  const createBook = (title) => {
+    const updatedBooks = [
+      ...books,
+      {
+        id: Math.round(Math.random() * 9999),
+        title: title
+      }
+    ];
+    setBooks(updatedBooks);
+  };
+
+
+  return (
+    <div className="app">
+      <h1>Reading List</h1>
+      <BookList books={books} onDelete={deleteBookById} onEdit={editBookById} />
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
+}
+
+export default App;
