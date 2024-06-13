31f9e0eecf6f88117ed3e96cbc531b6877442ce0
sarikayacem <79401289+sarikayacem@users.noreply.github.com>
2024-06-13 22:28:42 +0300

    table added

--- comps/src/App.js
+++ comps/src/App.js
@@ -4,6 +4,8 @@ import AccordionPage from "./pages/AccordionPage";
 import DropdownPage from "./pages/DropdownPage";
 import ButtonPage from "./pages/ButtonPage";
 import ModalPage from "./pages/ModalPages";
+import TablePage from "./pages/TablePage";
+import CounterPage from "./pages/CounterPage";
 
 function App() {
   return <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
@@ -21,6 +23,12 @@ function App() {
       </Route>
       <Route path="/modal">
         <ModalPage />
+      </Route>
+      <Route path="/table">
+        <TablePage />
+      </Route>
+      <Route path="/counter">
+        <CounterPage initialCount={10}/>
       </Route>
     </div>
   </div>
---
+++ comps/src/pages/TablePage.js
@@ -0,0 +1,41 @@
+import { render } from "@testing-library/react";
+import Table from "../components/Table";
+
+function TablePage() {
+
+    const data = [
+        { name: "Orange", color: "bg-orange-500", score: 5 },
+        { name: "Apple", color: "bg-red-500", score: 3 },
+        { name: "Banana", color: "bg-yellow-500", score: 1 },
+        { name: "Lime", color: "bg-green-500", score: 4 }
+    ];
+
+    const config = [
+        {
+            label: "Name",
+            render: (fruit) => fruit.name
+        },
+        {
+            label: "Color",
+            render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div>
+        },
+        {
+            label: "Score",
+            render: (fruit) => fruit.score,
+            header: ()=> <th className="bg-red-500">Score</th>
+
+        },
+    ];
+
+    const keyFN = (fruit) => {
+        return fruit.name;
+    };
+
+
+    return (<div>
+        <Table data={data} config={config} keyFN={keyFN} />
+    </div>
+    );
+}
+
+export default TablePage;
